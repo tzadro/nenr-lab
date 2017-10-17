853b24468aadddda895bb685d3b5592be86d017a
tzadro <tzadro94@gmail.com>
2017-10-17 23:44:57 +0200

    Added gamma function

--- src/hr/fer/tzadro/nenr/lab1/zad2/StandardFuzzySets.java
+++ src/hr/fer/tzadro/nenr/lab1/zad2/StandardFuzzySets.java
@@ -2,6 +2,21 @@ package hr.fer.tzadro.nenr.lab1.zad2;
 
 public class StandardFuzzySets {
 
+    public static IIntUnaryFunction gammaFunction(int alpha, int beta) {
+        return new IIntUnaryFunction() {
+
+            @Override
+            public double valueAt(int x) {
+                if (x < alpha)
+                    return 0;
+                else if (x >= alpha && x < beta)
+                    return 1. * (x - alpha) / (beta - alpha);
+                else
+                    return 1;
+            }
+        };
+    }
+
     public static IIntUnaryFunction lambdaFunction(int alpha, int beta, int gamma) {
         return new IIntUnaryFunction() {
 
