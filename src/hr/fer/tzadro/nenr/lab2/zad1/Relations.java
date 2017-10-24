1992761af68ac761bebd2fee0fa830899447d828
tzadro <tzadro@tzadro.home>
2017-10-24 20:44:22 +0200

    Lab2

---
+++ src/hr/fer/tzadro/nenr/lab2/zad1/Relations.java
@@ -0,0 +1,10 @@
+package hr.fer.tzadro.nenr.lab2.zad1;
+
+import hr.fer.tzadro.nenr.lab1.zad2.IFuzzySet;
+
+public class Relations {
+
+    public static boolean isUtimesURelation(IFuzzySet relation) {
+
+    }
+}
